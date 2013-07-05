50837ab929c835a3cfbd3a0842ae19e7eece89e1
Brian Detering <bdeterin@gmail.com>
2013-07-05 03:10:14 -0700

    first commit (allready feature complete)

---
+++ sql.php
@@ -0,0 +1,107 @@
+<?php
+//Wrapper to abstract the method of database access
+//(currently implented with PDO)
+class Sequel {
+    private $DB;
+
+    function __construct(array $fig = array()) {
+        $this->DB = $fig['connection'];
+    }
+
+    function select($query, array $values = array()) {
+        $statement = "SELECT $query";
+        $Results = $this->DB->prepare($statement);
+        $Results->execute($values);
+        return new Sequel_Results(array(
+            "results" => $Results,
+            "statement" => $statement,
+            "values" => $values,
+            "connection" => $this->DB
+        ));
+    }
+
+    function insert($query, array $values = array()) {
+        $this->DB->prepare("INSERT INTO $query")->execute($values);
+        return $this->DB->lastInsertId();
+    }
+
+    function update($query, array $values = array()) {
+        $this->DB->prepare("UPDATE $query")->execute($values);
+    }
+
+    function delete($query, array $values = array()) {
+        $this->DB->prepare("DELETE FROM $query")->execute($values);
+    }
+}
+
+
+//Results Set Wrapper returned by calls to select
+class Sequel_Results implements Iterator {
+    private $Results,
+            $DB,
+            $predicate,
+            $values,
+            $count = null,
+            $key = -1,
+            $current;
+
+    function __construct(array $fig = array()) {
+        $this->Results = $fig['results'];
+        $this->predicate = $this->extract_select_predicate($fig['statement']);
+        $this->values = $fig['values'];
+        $this->DB = $fig['connection'];
+
+        $this->Results->setFetchMode(PDO::FETCH_ASSOC);
+        $this->next();
+    }
+
+    private function extract_select_predicate($query) {
+        return substr($query, strpos($query, "FROM"));
+    }
+
+    function to_array() {
+        $arrayResults = array();
+        while($row = $this->next()) {
+            $arrayResults[] = $row;
+        }
+        return $arrayResults;
+    }
+
+    //rowCount doesnt work for sqlite :(
+    function count() {
+        if($this->count === null) {
+            $sql= "SELECT count(*) " . $this->predicate;
+            $sth = $this->DB->prepare($sql);
+            $sth->execute($this->values);
+            $rows = $sth->fetch(\PDO::FETCH_NUM);
+            $this->count = $rows[0];
+        }
+        return $this->count;
+    }
+
+    function rewind() {
+        if($this->key !== 0) {
+            throw new Exception("Sequel_Results does not support rewind.");
+        }
+    }
+
+    function valid() {
+        return ($this->current !== false);
+    }
+
+    function current() {
+        return $this->current;
+    }
+
+    function key() {
+        return $this->key;
+    }
+
+    function next() {
+        $this->key += 1;
+        $hold = $this->current;
+        $this->current = $this->Results->fetch();
+        return $hold;
+    }
+}
+?>
---
+++ sqlTest.php
@@ -0,0 +1,125 @@
+<?php
+require_once "sql.php";
+
+class Sql_Test extends PHPUnit_Framework_TestCase {
+
+    private $Sql, $DB;
+
+    function setUp() {
+        $this->DB = new PDO("sqlite::memory:");
+        $this->Sql = new Sequel(array(
+            "connection" => $this->DB
+        ));
+        $this->create_database();
+        $this->insert_default_rows();
+    }
+
+    private function create_database() {
+        $this->DB->exec(
+            "CREATE TABLE IF NOT EXISTS A (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                a CHAR(3),
+                b INT
+            )"
+        );
+    }
+
+    private function insert_default_rows() {
+        $this->DB->prepare("INSERT INTO A (id, a, b) VALUES (?, ?, ?)")->execute(array(1, "foo", 5));
+        $this->DB->prepare("INSERT INTO A (id, a, b) VALUES (?, ?, ?)")->execute(array(2, "bar", 6));
+    }
+
+
+    function test_results_count() {
+        $Results = $this->Sql->select("* FROM A");
+        $this->assertEquals(2, $Results->count());
+    }
+
+    function test_results_next() {
+        $Results = $this->Sql->select("* FROM A");
+        $this->assertEquals(array("id" => 1,"a" => "foo", "b" => 5), $Results->next());
+    }
+
+    function test_results_foreach_loop() {
+        $actualResults = array();
+        foreach($this->Sql->select("* FROM A") as $key => $val) {
+            $actualResults[$key] = $val;
+        }
+
+        $this->assertEquals(
+            array(
+                array("id" => 1, "a" => "foo", "b" => 5),
+                array("id" => 2,"a" => "bar", "b" => 6)
+            ),
+            $actualResults
+        );
+    }
+
+    function test_results_foreach_loop_empty_results() {
+        $ResultsObject = $this->Sql->select("* FROM A WHERE a = ?", array("wrong"));
+        $actualResults = array();
+        foreach($ResultsObject as $key => $val) {
+            $actualResults[$key] = $val;
+        }
+        $this->assertEquals(array(), $actualResults);
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage Sequel_Results does not support rewind.
+     */
+    function test_results_foreach_loop_next_allready_called() {
+        $ResultsObject = $this->Sql->select("* FROM A");
+        $ResultsObject->next();
+        $actualResults = array();
+        foreach($ResultsObject as $key => $val) {
+            $actualResults[$key] = $val;
+        }
+    }
+
+    function test_insert() {
+        $this->Sql->insert("A (id, a, b) VALUES (? ,?, ?)", array(3, "baz", 7));
+        $Results = $this->DB->query("SELECT * FROM A WHERE id='3'");
+        $Results->setFetchMode(PDO::FETCH_ASSOC);
+        $this->assertEquals(
+            array("id" => 3, "a" => "baz", "b" => 7),
+            $Results->fetch()
+        );
+    }
+
+    function test_insert_id() {
+        $id = $this->Sql->insert("A (id, a, b) VALUES (? ,?, ?)", array(3, "baz", 7));
+        $this->assertEquals(3, $id);
+    }
+
+    function test_update() {
+        $this->Sql->update("A SET a = 'edit' WHERE id='1'");
+        $Results = $this->DB->query("SELECT * FROM A WHERE id='1'");
+        $Results->setFetchMode(PDO::FETCH_ASSOC);
+        $this->assertEquals(
+            array("id" => 1, "a" => "edit", "b" => 5),
+            $Results->fetch()
+        );
+    }
+
+    function test_delete() {
+        $this->Sql->delete("A WHERE id='1'");
+        $Results = $this->DB->query("SELECT * FROM A WHERE id='1'");
+        $this->assertEquals(
+            false,
+            $Results->fetch()
+        );
+    }
+
+    function test_results_to_array() {
+        $ResultsObject = $this->Sql->select("* FROM A");
+        $this->assertEquals(
+            array(
+                array("id" => 1, "a" => "foo", "b" => 5),
+                array("id" => 2,"a" => "bar", "b" => 6)
+            ),
+            $ResultsObject->to_array()
+        );
+    }
+}
+?>
